d5d69a7de10f2566ceca5db5abe958bdffa800fc
AnushaReddi02 <anushareddi115@gmail.com>
2026-02-09 08:55:36 +0530

    Using Utility Middleware - Logger functions

--- app.js
+++ app.js
@@ -3,17 +3,25 @@ const app = express();
 
 
 
-app.use((req,res,next)=>{
-    console.log("Hey....I am a 1st MIDDLEWARE");
-    // res.send("Middleware finished");
-    next();
-});
+// app.use((req,res,next)=>{
+//     console.log("Hey....I am a 1st MIDDLEWARE");
+//     // res.send("Middleware finished");
+//     next();
+// });
 
+// app.use((req,res,next)=>{
+//     console.log("Hey....I am a 2nd MIDDLEWARE");
+//     // res.send("Middleware finished");
+//     next();
+// });
+
+
+//Logger M/W - Morgan is built-in M/W it doea this same work.
 app.use((req,res,next)=>{
-    console.log("Hey....I am a 2nd MIDDLEWARE");
-    // res.send("Middleware finished");
+    req.time = new Date(Date.now()).toString();
+    console.log(req.host,req.method,req.path,req.time);
     next();
-});
+})
 
 app.get("/",(req,res)=>{
     res.send("Hey...I am ROOT");
